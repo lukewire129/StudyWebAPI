ce511d28799547eafa5304d4640e0bdb90536c7e
lukewire <lukewire129@gmail.com>
2024-02-06 15:30:31 +0900

    Log Add

---
+++ src/3.LogAdd/IMS/Program.cs
@@ -0,0 +1,56 @@
+using IMS.database.context;
+using NLog.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ImsContext>();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders(); // 기본 로깅 프로바이더를 지웁니다.
+    loggingBuilder.SetMinimumLevel(LogLevel.Trace); // 로그 레벨 설정
+    loggingBuilder.AddNLog(); // NLog를 사용하도록 설정
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ src/3.LogAdd/IMS/database/context/ImsContext.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IMS.database.entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.database.context;
+
+public class ImsContext : DbContext
+{
+    private ILogger<ImsContext> _logger;
+
+    public DbSet<IdolGroup> IdolGroup { get; set; }
+    public DbSet<Member> Member { get; set; }
+    public ImsContext(DbContextOptions<ImsContext> options, ILogger<ImsContext> logger)
+        : base(options)
+    {
+        _logger = logger;
+    }
+
+
+    protected ImsContext()
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var dbPath = Path.Combine(Directory.GetCurrentDirectory(), "ims.db");
+
+        // SQLite 데이터베이스 연결을 설정합니다.
+        optionsBuilder.UseSqlite($"Data Source={dbPath}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<IdolGroup>()
+                    .HasMany(ig => ig.Members)
+                    .WithMany(m => m.IdolGroups)
+                    .UsingEntity<IdolGroupLinkMember>(
+                    builderLink => builderLink
+                    .HasOne<Member>()
+                    .WithMany()
+                    .HasForeignKey(iglm => iglm.MemberId),
+                    builderLink => builderLink
+                    .HasOne<IdolGroup>()
+                    .WithMany()
+                    .HasForeignKey(iglm => iglm.IdolGroupId),
+                builderLink =>
+                {
+                    builderLink.HasKey(iglm => new { iglm.IdolGroupId, iglm.MemberId }); // 복합 키 설정
+                });
+    }
+}
